01a5cdd881b8c8a2266bbe821df59b70ed6a019d
Admin <admin>
2016-01-15 21:27:30 +0200

    db settings

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=87.236.19.3;port=33060;dbname=leksmiks2_world',
+    'emulatePrepare' => true,
+    'username' => 'leksmiks2_world',
+    'password' => 'max5549048965ned',
     'charset' => 'utf8',
 ];
